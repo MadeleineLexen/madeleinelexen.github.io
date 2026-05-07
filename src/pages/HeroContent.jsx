0bf7d94b0e8fc4e7c82d16541ad3afe9b471103a
Madeleine Lexén <madeleine.lexen@stenaline.com>
2026-05-07 14:26:00 +0200

    Removed scrambled word heading in hero

--- src/pages/HeroContent.jsx
+++ src/pages/HeroContent.jsx
@@ -7,10 +7,7 @@ const HeroContent = () => {
       <div className="hero-content hero-content-left">
         <h1 className="hero-heading left">Hi, I&apos;m Madeleine,</h1>
         <h1 className="hero-heading left">
-          <ClarifyingHeadline 
-            before="Desgiingn clrtaiy " 
-            after="Designing clarity "
-          /> for data-driven products people can trust.
+          Designing clarity for data-driven products people can trust.
         </h1>
         <p className="hero-subheading left">
           Nice of you to look in. This is a space where I share a bit of my work and thoughts on design, data, and the human experience.
